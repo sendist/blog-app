fb86d02c70ca7532125fd698750bd30b869a2913
sendist <sendisetiawan10902@gmail.com>
2025-12-22 17:52:08 +0700

    fix(backend): enable CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,10 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
